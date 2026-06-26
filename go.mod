64628d1ba2646ad18fa9f21331e7ff00983af98f
Prajwal Benedict Norman <normanprajwal@gmail.com>
2026-06-27 01:55:47 +0530

    init config.go file

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module QueryGuard
 
 go 1.25.3
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/goccy/go-yaml v1.19.2
+	github.com/gorilla/mux v1.8.1
+	gopkg.in/yaml.v3 v3.0.1
+)
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,80 @@
 package config
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Config struct{
+		Server struct{
+			Port int `yaml:"port"`
+		} `yaml:"server"`
+		Metrics struct {
+			CUSUM struct{
+				K float32 `yaml:"k"`
+				H float32 `yaml:"h"`
+				Mean float32 `yaml:"mean"`
+				Variance float32 `yaml:"variance"`
+			}`yaml:"cusum"`
+			EWMA struct{
+				Lambda float32 `yaml:"lambda"`
+				StdDev float32 `yaml:"std_dev"`
+				Mean float32 `yaml:"mean"`
+				Threshold float32 `yaml:"threshold"`
+			}`yaml:"ewma"`
+		}`yaml:"metrics"`
+		Target struct{
+			URL string `yaml:"url"`
+			Method string `yaml:"method"`
+		}`yaml:"target"`
+		Request struct{
+			Header  map[string]string `yaml:"header"`
+			Body struct {
+				Message string `yaml:"message"`
+				SessionId string `yaml:"session_id"`
+			}`yaml:"body"`
+		}`yaml:"request"`
+		Response struct{
+				Path string `yaml:"path"`
+		}`yaml:"response"`		
+		Baseline struct{
+			Model string `yaml:"model"`
+			Threshold float64 `yaml:"threshold"`
+			Store string `yaml:"store"`
+		}`yaml:"baseline"`
+
+		Cache struct {
+			Size int `yaml:"size"`
+		}`yaml:"cache"`
+
+}
+
+func Load() (*Config,error){
+	data,err :=os.ReadFile("config.yaml")
+	if err !=nil{
+		log.Fatal("No config file found")
+		return  nil,err
+	}
+	var cfg Config
+	if err := yaml.Unmarshal(data,&cfg);err !=nil{
+		return  nil,err
+	}
+	// Url finding
+	if cfg.Target.URL == ""{
+		fmt.Print("Enter the model url")
+		reader :=bufio.NewReader(os.Stdin)
+		url,_:=reader.ReadString('\n')
+		cfg.Target.URL=strings.TrimSpace(url)
+	}
+
+	// Loading evn values
+	for key, value := range cfg.Request.Header {
+    cfg.Request.Header[key] = os.Expand(value, os.Getenv)
+}
+	return &cfg,nil
+}
